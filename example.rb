38aeb76c39b7d25697e6ec265e16cce8053be8ed
John Mair <jrmair@gmail.com>
2010-09-27 10:20:29 +1300

    updated example.rb

--- example.rb
+++ example.rb
@@ -1,18 +1,18 @@
 require './lib/custom_boolean'
 
 x = 7
-if!(x == 4) {
+if?(x == 4) {
   puts 'x is 4' 
 }.
-else_if!(x == 5) {
+else_if?(x == 5) {
   puts "x is #{x} (first else_if)"
 }.
-else_if!(x == 7) {
+else_if?(x == 7) {
   puts "x is #{x} (second else_if)"
 }.
-else_if!(x == 10) {
+else_if?(x == 10) {
   puts 'x is #{x} (third else_if)'
 }.
-else! {
+else? {
   puts 'reached else'
 }
