19e29157b0093cbc8cc3d0d0eaa7f89938ef3872
John Mair <jrmair@gmail.com>
2010-09-27 09:58:15 +1300

    bunch of tests, and removed extraneous comments

--- lib/custom_boolean.rb
+++ lib/custom_boolean.rb
@@ -21,13 +21,14 @@ class CustomBoolean
   # exception raised when conditionals invoked after an else
   InvalidConditional = Class.new(StandardError)
   
-  # default truth test (same as Ruby's)
+  # various ideas of truth
   RUBY_TRUTH = proc { |expr| expr }
   PYTHON_TRUTH = proc { |expr| expr && !(expr.respond_to?(:empty?) && expr.empty?) && expr != 0 }
   STRICT_TRUTH = proc { |expr| raise "Expression must be strictly true or false." if expr != true && expr != false; expr }
   PERL_TRUTH = proc { |expr| expr && expr != 0 && expr != "" && expr != "0" }
   C_TRUTH = proc { |expr| expr && expr != 0 }
     
+  # default truth test is Ruby's
   self.truth_test = RUBY_TRUTH
   
   def initialize(truth_value, block_value)
@@ -40,22 +41,15 @@ class CustomBoolean
 
     if self.truth_value
 
-      # current conditional is true so just pass on truth and do not
-      # run block.
       CustomBoolean.new(true, self.block_value)
     else
-
-      # current conditional is false so check next conditional for truth
       truth = !!CustomBoolean.truthy?(condition)
-
-      # run block if next conditional is true
       bvalue = block.call if truth
-
-      # pass on new truth value
       CustomBoolean.new(truth, bvalue)
     end
   end
 
+  # a bunch of aliases for else if
   alias_method :elsif!, :else_if!
   alias_method :elif!, :else_if!
   alias_method :elseif!, :else_if!
